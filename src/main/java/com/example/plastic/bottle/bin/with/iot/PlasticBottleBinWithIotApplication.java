b3fa31f086a246b903df212cc159cdf7b63b856e
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-05-04 02:59:59 +0000

    project initialized

---
+++ src/main/java/com/example/plastic/bottle/bin/with/iot/PlasticBottleBinWithIotApplication.java
@@ -0,0 +1,13 @@
+package com.example.plastic.bottle.bin.with.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlasticBottleBinWithIotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlasticBottleBinWithIotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/plastic/bottle/bin/with/iot/PlasticBottleBinWithIotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plastic.bottle.bin.with.iot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlasticBottleBinWithIotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
