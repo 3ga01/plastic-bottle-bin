d1357bc82b44e2af59eb4c2095352fd94aca8def
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-08-06 07:41:24 +0000

    bin fill updated

--- src/main/java/com/example/plastic/bottle/bin/with/iot/Controller/Controller.java
+++ src/main/java/com/example/plastic/bottle/bin/with/iot/Controller/Controller.java
@@ -3,13 +3,10 @@ package com.example.plastic.bottle.bin.with.iot.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
